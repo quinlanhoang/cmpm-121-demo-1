8ea5b47ce75dd4bda0d2b95f2237c7bdd9731fba
Quinlan <qbhoang@ucsc.edu>
2024-10-08 15:23:43 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Quinlan's Game";
+const gameName = "That's Hot";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//button element
+const button = document.createElement("button");
+button.innerText = "🔥";
+
+button.addEventListener("click", () => {
+    alert(`You're on fire!`);
+});
+
+app.append(button);
