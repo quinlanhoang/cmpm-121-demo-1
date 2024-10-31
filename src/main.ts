05afa1b2cd466c1793335a435fa338c8f55c14db
Quinlan <qbhoang@ucsc.edu>
2024-10-31 14:07:29 -0700

    Code Revision 2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import './style.css';
-import items from './items.json'; // Importing the items from the JSON file
+import "./style.css";
+import items from "./items.json"; // Importing the items from the JSON file
 
 // Constants
 const GAME_NAME = "That's Hot";
@@ -144,12 +144,18 @@ function updateItemButtonState(button: HTMLButtonElement, index: number) {
 
 function getUpgradeTooltip(name: string): string {
   switch (name) {
-    case "Matches": return "A quick firestarter!";
-    case "Campfires": return "Lets make some s'mores!";
-    case "Stovetops": return "Lets cook something HOT!";
-    case "Pyro": return "It's like a flamethrower.";
-    case "Fireball": return "Too hot to handle.";
-    default: return "Upgrade your fire power!";
+    case "Matches":
+      return "A quick firestarter!";
+    case "Campfires":
+      return "Lets make some s'mores!";
+    case "Stovetops":
+      return "Lets cook something HOT!";
+    case "Pyro":
+      return "It's like a flamethrower.";
+    case "Fireball":
+      return "Too hot to handle.";
+    default:
+      return "Upgrade your fire power!";
   }
 }
 
@@ -174,7 +180,9 @@ function animate(currentTimestamp: number) {
     incrementCounter(growthRate * elapsed);
     flameButton.style.transform = `scale(${1 + count * 0.02})`;
 
-    itemButtons.forEach((button, index) => updateItemButtonState(button, index));
+    itemButtons.forEach((button, index) =>
+      updateItemButtonState(button, index),
+    );
   }
   lastTimestamp = currentTimestamp;
   requestAnimationFrame(animate);
